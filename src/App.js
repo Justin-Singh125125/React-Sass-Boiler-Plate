37ecaeb7d743f655c1efd3795bfb2079e7bced85
Justin Singh <justin.singh125125@gmail.com>
2019-06-14 23:25:22 -0700

    added react router dom

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
-import Card from "./components/Card";
+import React from "react";
+import Home from "./pages/Home";
+import { Route, Switch } from "react-router-dom";
+import "./App.scss";
 
-function App() {
+
+const App = () => {
+
+
   return (
-    <div className="App">
-
-      <header className="App-header">
-        <Card />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      {/* these are good */}
+      <Route exact path="/" component={Home} />
+    </Switch>
   );
-}
-
+};
 export default App;
