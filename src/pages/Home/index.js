ecd2ca30b88f59e036d5e8283ed56aa2aafb4833
Justin Singh <justin.singh125125@gmail.com>
2019-07-08 10:31:16 -0700

    made some changes to boilerplate

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,30 +1,11 @@
-import React, { Component } from 'react';
-import Card from "../../components/Card";
-import logo from '../../logo.svg';
+import React from "react";
 
-export default class Home extends Component {
+
+
+class Home extends React.Component {
     render() {
-        return (
-            <div>
-
-
-                <header className="App-header">
-                    <Card />
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                        </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
-            </div>
-
-        )
+        return "";
     }
 }
+
+export default Home;
